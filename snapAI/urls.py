7072b4fb1c33e47cf158833fdd3bc84dc6975770
sattarzhanovdev <sattarzhanovdev@gmail.com>
2025-09-27 16:10:47 +0600

    push

--- snapAI/urls.py
+++ snapAI/urls.py
@@ -1,11 +1,16 @@
+from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("api/", include("api.urls")),  # твои API-роуты
 
-    path("admin/", include("django.contrib.admin.urls")),  # админка
+    path("admin/", admin.site.urls),  # стандартная админка
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/docs/",   SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/redoc/",  SpectacularRedocView.as_view(url_name="schema"),   name="redoc"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
